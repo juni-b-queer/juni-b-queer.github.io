736d37c68fc38e555df621295be31ac3145b8eb9
Juni <june@juniper-bender.tech>
2025-01-01 20:20:00 -0600

    wahoo

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,6 +9,9 @@ export default defineComponent({
 <template>
   <div>
     <h1>Bluesky team activity tracker</h1>
-    <p>This is an off-site Bluesky activity tracker for members of the Bluesky team.</p>
+    <p>This is an off-site Bluesky activity tracker for members of the Bluesky team, purely for transparency.</p>
+    <p>It does not show deleted posts, unfollows, removed likes, etc. It only shows what is easily accessible from the public Bluesky API</p>
+    <p>If you would like to be removed from this, all you have to do is tag or message @literallyjustadog.online on Bluesky :)</p>
+
   </div>
 </template>
--- src/views/TeamMembers.vue
+++ src/views/TeamMembers.vue
@@ -5,6 +5,7 @@ import ImageGrid from '@/components/ImageGrid.vue'
 <template>
   <div>
     <h1>Bluesky team activity tracker</h1>
+    <p>If you would like to be removed from this, all you have to do is tag or message @literallyjustadog.online on bluesky :)</p>
 
     <ImageGrid />
   </div>
