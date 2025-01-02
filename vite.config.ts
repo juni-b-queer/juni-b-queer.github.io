169b83cdfd5ecfb5cd2cb78df3b37dfdb98776ef
Juni <june@juniper-bender.tech>
2025-01-01 19:28:50 -0600

    update

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/juni-b-queer.github.io/",
+  // base: "/juni-b-queer.github.io/",
   plugins: [
     vue(),
     vueDevTools(),
