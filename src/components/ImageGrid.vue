aa03af78ebf6d395d334a15f87bc8c8dabce0e9d
Juni <june@juniper-bender.tech>
2025-01-02 12:07:10 -0600

    fix mobile view

--- src/components/ImageGrid.vue
+++ src/components/ImageGrid.vue
@@ -19,24 +19,22 @@ export default defineComponent({
     return {
       teamDids: [
         { did: 'did:plc:wpp4lklhvmopw6zcy6qb42ru' },
-        { did: 'did:plc:oky5czdrnfjpqslsw2a5iclo' },
         { did: 'did:plc:ksjfbda7262bbqmuoly54lww' },
+        { did: 'did:plc:oky5czdrnfjpqslsw2a5iclo' },
         { did: 'did:plc:qjeavhlw222ppsre4rscd3n2' },
-        { did: 'did:plc:ragtjsm2j2vknwkz3zp4oxrd' },
-        { did: 'did:plc:3jpt2mvvsumj2r7eqk4gzzjz' },
-        { did: 'did:plc:vjug55kidv6sye7ykr5faxxn' },
-        { did: 'did:plc:p2cp5gopk7mgjegy6wadk3ep' },
         { did: 'did:plc:linrigsaay5zenhg756ca6tg' },
         { did: 'did:plc:tl7zqgil2irwndwojsxszceb' },
-        { did: 'did:plc:oisofpd7lj26yvgiivf3lxsi', pds: 'https://pds.haileyok.com' },
+        { did: 'did:plc:vpkhqolt662uhesyj6nxm7ys' },
+        { did: 'did:plc:3jpt2mvvsumj2r7eqk4gzzjz' },
         { did: 'did:plc:44ybard66vv44zksje25o7dz' },
         { did: 'did:plc:yk4dd2qkboz2yv6tpubpc6co', pds: 'https://pds.dholms.xyz' },
-        { did: 'did:plc:vpkhqolt662uhesyj6nxm7ys' },
-        { did: 'did:plc:q6gjnaw2blty4crticxkmujt' },
         { did: 'did:plc:vzmlifz3ghar4cu2hj3srga2' },
         { did: 'did:plc:l3rouwludahu3ui3bt66mfvj' },
-
-
+        { did: 'did:plc:vjug55kidv6sye7ykr5faxxn' },
+        { did: 'did:plc:ragtjsm2j2vknwkz3zp4oxrd' },
+        { did: 'did:plc:p2cp5gopk7mgjegy6wadk3ep' },
+        { did: 'did:plc:q6gjnaw2blty4crticxkmujt' },
+        { did: 'did:plc:oisofpd7lj26yvgiivf3lxsi', pds: 'https://pds.haileyok.com' },
       ],
     }
   },
